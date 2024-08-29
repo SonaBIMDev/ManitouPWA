6291ea0b30bda36fed16ca03986a9d4b93f767a9
Pierre Navarra <p.navarra@sona-architecture.com>
2024-08-29 18:45:24 +0200

    googleapis

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,30 +1,50 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { google } = require('googleapis');
 
 exports.handler = async (event, context) => {
   try {
     console.log("Début de la fonction getData");
-    
-    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID_FROM_URL);
-    console.log("GoogleSpreadsheet initialisé");
 
-    console.log("Email du compte de service:", process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL);
-    console.log("ID de la feuille de calcul:", process.env.GOOGLE_SPREADSHEET_ID_FROM_URL);
-    
-    const credentials = {
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    };
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      },
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    });
 
-    await doc.useServiceAccountAuth(credentials);
-    console.log("Authentification réussie");
+    console.log("Auth créé");
 
-    // Le reste de votre code...
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    console.log("Client Sheets créé");
+
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID_FROM_URL,
+      range: 'COORDINATES!A2:E',
+    });
+
+    console.log("Données récupérées");
+
+    const rows = response.data.values;
+    if (rows.length) {
+      console.log("Données trouvées");
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true, data: rows[0] })
+      };
+    } else {
+      console.log("Aucune donnée trouvée");
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ success: false, message: "Aucune donnée trouvée" })
+      };
+    }
 
   } catch (error) {
     console.error('Erreur générale dans getData:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ success: false, error: error.message }),
+      body: JSON.stringify({ success: false, error: error.message })
     };
   }
 };
