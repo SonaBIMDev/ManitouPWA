a71f99b096160aed6c92c35a491e8d5462bfd580
Pierre Navarra <p.navarra@sona-architecture.com>
2024-08-29 19:17:56 +0200

    test2

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -2,14 +2,25 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 exports.handler = async (event, context) => {
     try {
-      console.log("Début de la fonction getData");
-    
-    } catch (error) {
-    console.error('Erreur générale dans getData:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ success: false, error: error.message })
-    };
+        console.log("Début de la fonction getData");
+
+        const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID_FROM_URL);
+
+        // https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+        await doc.useServiceAccountAuth({
+            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        });
+        await doc.loadInfo(); // loads document properties and worksheets. required.
+        const sheet = doc.sheetsByIndex[0]; // you may want to customize this if you have more than 1 sheet
+        // console.log('accessing', sheet.title, 'it has ', sheet.rowCount, ' rows');
+        
+        } catch (error) {
+        console.error('Erreur générale dans getData:', error);
+        return {
+        statusCode: 500,
+        body: JSON.stringify({ success: false, error: error.message })
+        };
   }
 };
 
