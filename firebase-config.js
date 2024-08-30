84ae6d69d8a633000c0f09da66f5de3ded3b0296
Pierre Navarra <p.navarra@sona-architecture.com>
2024-08-30 14:43:36 +0200

    firebase

---
+++ firebase-config.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAgSNLgxsqFDTIoOsoMtn-Fm-7T3ixdfiE",
+
+  authDomain: "manitou-a2370.firebaseapp.com",
+
+  databaseURL: "https://manitou-a2370-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "manitou-a2370",
+
+  storageBucket: "manitou-a2370.appspot.com",
+
+  messagingSenderId: "510602875968",
+
+  appId: "1:510602875968:web:d475699a18cc65c687bba5"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,91 +1,39 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { initializeApp } = require('firebase/app');
+const { getDatabase, ref, get } = require('firebase/database');
+const firebaseConfig = require('./firebase-config');
 
-exports.handler = async (event, context) => {
-    try {
-        console.log("Début de la fonction getData");
-        console.log("GOOGLE_SPREADSHEET_ID_FROM_URL:", process.env.GOOGLE_SPREADSHEET_ID_FROM_URL);
-        console.log("GOOGLE_SERVICE_ACCOUNT_EMAIL:", process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL);
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
-        const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID_FROM_URL);
-        console.log("GoogleSpreadsheet initialisé");
-
-        await doc.useServiceAccountAuth({
-            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-        });
-        console.log("Authentification réussie");
-
-        await doc.loadInfo();
-        console.log("Informations du document chargées");
-
-        const sheet = doc.sheetsByIndex[0];
-        console.log('Accès à la feuille:', sheet.title, 'elle a', sheet.rowCount, 'lignes');
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ success: true, message: "Connexion à la Google Sheet réussie" })
-        };
-    } catch (error) {
-        console.error('Erreur générale dans getData:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ success: false, error: error.message })
-        };
-    }
-};
-
-
-/*
-const { google } = require('googleapis');
-
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   try {
-    console.log("Début de la fonction getData");
+    const { elementId } = JSON.parse(event.body);
+    const snapshot = await get(ref(database, `elements/${elementId}`));
 
-    const privateKey = Buffer.from(process.env.GOOGLE_PRIVATE_KEY, 'base64').toString('ascii');
-
-    const auth = new google.auth.GoogleAuth({
-      credentials: {
-        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: privateKey,
-      },
-      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-    });
-
-    console.log("Auth créé");
-
-    const sheets = google.sheets({ version: 'v4', auth });
-
-    console.log("Client Sheets créé");
-
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID_FROM_URL,
-      range: 'COORDINATES!A2:E',
-    });
-
-    console.log("Données récupérées");
-
-    const rows = response.data.values;
-    if (rows && rows.length) {
-      console.log("Données trouvées");
+    if (snapshot.exists()) {
+      const data = snapshot.val();
       return {
         statusCode: 200,
-        body: JSON.stringify({ success: true, data: rows[0] })
+        body: JSON.stringify({
+          success: true,
+          elementid: data.elementid,
+          latitude: data.latitude,
+          longitude: data.longitude,
+          commentaire: data.commentaire,
+          google_maps: data.google_maps
+        })
       };
     } else {
-      console.log("Aucune donnée trouvée");
       return {
         statusCode: 404,
-        body: JSON.stringify({ success: false, message: "Aucune donnée trouvée" })
+        body: JSON.stringify({ success: false, message: 'Données non trouvées' })
       };
     }
-
   } catch (error) {
-    console.error('Erreur générale dans getData:', error);
+    console.error('Erreur:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ success: false, error: error.message })
+      body: JSON.stringify({ success: false, error: 'Erreur serveur' })
     };
   }
 };
-*/
